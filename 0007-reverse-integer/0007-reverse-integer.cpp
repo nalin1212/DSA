a1da8ea787d3e653412f778e8ac594c55316e874
Nalin Tiwari <145797322+nalin1212@users.noreply.github.com>
2026-09-11 05:57:07 +0530

    Time: 0 ms (100%), Space: 8.5 MB (83.41%) - LeetHub

--- 0007-reverse-integer/0007-reverse-integer.cpp
+++ 0007-reverse-integer/0007-reverse-integer.cpp
@@ -4,15 +4,17 @@ public:
         int rev = 0;
 
         while (x != 0) {
-            int ld = x % 10;
+            int digit = x % 10;
 
-            if (rev > INT_MAX / 10 || (rev == INT_MAX / 10 && ld > 7))
+            // Check for overflow
+            if (rev > INT_MAX / 10 || (rev == INT_MAX / 10 && digit > 7))
                 return 0;
 
-            if (rev < INT_MIN / 10 || (rev == INT_MIN / 10 && ld < -8))
+            // Check for underflow
+            if (rev < INT_MIN / 10 || (rev == INT_MIN / 10 && digit < -8))
                 return 0;
 
-            rev = rev * 10 + ld;
+            rev = rev * 10 + digit;
             x /= 10;
         }
 
