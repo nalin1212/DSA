a71acde7a34665089e29e494a8021f59bdfd1009
Nalin Tiwari <145797322+nalin1212@users.noreply.github.com>
2026-09-19 20:23:44 +0530

    Time: 0 ms (100%), Space: 7.9 MB (5%) - LeetHub

---
+++ 1401-circle-and-rectangle-overlapping/1401-circle-and-rectangle-overlapping.cpp
@@ -0,0 +1,8 @@
+class Solution {
+public:
+    bool checkOverlap(int r, int xC, int yC, int x1, int y1, int x2, int y2) { 
+        int x=clamp(xC,x1,x2)-xC;
+        int y=clamp(yC,y1,y2)-yC;
+        return x*x + y*y <= r*r;}
+
+};
